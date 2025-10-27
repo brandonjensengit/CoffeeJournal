2e998062ffc8953b4da818d7505fe6653f6fc00c
brandonjensengit <brandon@brandonjensen.info>
2025-10-26 18:21:43 -0600

    Initial Commit

---
+++ coffeeJournal/coffeeJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeJournalApp.swift
+//  coffeeJournal
+//
+//  Created by Brandon Jensen on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct coffeeJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coffeeJournalTests/coffeeJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeJournalTests.swift
+//  coffeeJournalTests
+//
+//  Created by Brandon Jensen on 10/26/25.
+//
+
+import Testing
+@testable import coffeeJournal
+
+struct coffeeJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
